34647a70256914c9b7e2aef5225aa22d1985f493
Denver <70090559+Denver44@users.noreply.github.com>
2021-11-24 17:42:09 +0530

    Fundamentals Part 1 Linking Js file Done

---
+++ JavaScript/1JavaScript_Fundamentals_Part_1/1Linking_a_JavaScript_file/script.js
@@ -0,0 +1,2 @@
+console.log("HELLO WORLD EXTERNAL LINKING ");
+console.log(5 * 4 + 2 + 3 * 6);
--- JavaScript/1LinkingJs/main.js
+++
@@ -1,2 +0,0 @@
-document.write("<h1> HELLO WORLD EXTERNAL LINKING </h1>");
-console.log("welcome to JS world");
