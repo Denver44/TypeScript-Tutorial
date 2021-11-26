7acc667c00efbdd08e26f207e946dc991c505ab7
Denver <70090559+Denver44@users.noreply.github.com>
2021-11-26 17:27:16 +0530

    Loops Concept in Js

--- JavaScript/2JavaScript_Fundamentals_Part_2/6Iteration_the_forLoop/script.js
+++ JavaScript/2JavaScript_Fundamentals_Part_2/6Iteration_the_forLoop/script.js
@@ -4,60 +4,64 @@
 //3. do-while
 
 // FOR LOOP
-// for (let i = 0; i < 100; i++) {
-//     console.log(i);
-// }
+
+for (let i = 0; i < 100; i++) {
+  if (i === 80) {
+    break;
+  }
+  console.log(i);
+}
 
 // WHILE LOOP
 
-// let j = 0;
-// while (j < 10) {
-//   console.log(j);
-//   j++;
-// }
+let j = 0;
+while (j < 10) {
+  console.log(j);
+  j++;
+}
 
 // DO WHILE LOOP
 
-// let k = 0;
-// do {
-//     if (k === 5) {
-//         k++;
-//         continue; // this is for skip that part.
-//     }
-//     console.log(k);
-//     k++;
-// } while (k < 10);
+let k = 0;
+do {
+  if (k === 5) {
+    k++;
+    continue; // this is for skip that part.
+  }
+  console.log(k);
+  k++;
+} while (k < 10);
 
-// let arr = [2, 5, 6, 4, 2, 3];
+let arr = [2, 5, 6, 4, 2, 3];
 
 // EXERCISE
 
 //  OLD METHOD TO PRINT THE LOOP ELEMENTS
-// for (let index = 0; index < arr.length; index++) {
-//     const element = arr[index];
-//     console.log(element);
-// }
+for (let index = 0; index < arr.length; index++) {
+  const element = arr[index];
+  console.log(element);
+}
 
 // FOREACH FUNCTION NEW METHOD TO PRINT THE ARRAY ELEMENT
 
-// arr.forEach(function(element, index, array){
-//     console.log(element, index, array);
-// });
+arr.forEach(function (element, index, array) {
+  console.log(element, index, array);
+});
 
-// let obj = {
-//     name: "Rohan Das",
-//     age: 78,
-//     type: "Dangerous Programmer",
-//     os: "Ubuntu"
-// }
+let obj = {
+  name: "Rohan Das",
+  age: 78,
+  type: "Dangerous Programmer",
+  os: "Ubuntu",
+};
 
 //  TO PRINT THE OBJECT ELEMENT
 
-// for (let key in obj) {
-//     console.log(`The ${key} of object is ${obj[key]}`)
-// }
+for (let key in obj) {
+  console.log(`The ${key} of object is ${obj[key]}`);
+}
 
-// console.log('done');
+console.log("done");
 
 let res = 0;
 for (let index = 1; index <= 10; index++) {
